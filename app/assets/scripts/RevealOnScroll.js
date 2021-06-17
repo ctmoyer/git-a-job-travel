471d515548201ca566781e56ab73968beeeecb46
ctmoyer <ctmoyer90@gmail.com>
2021-06-17 13:30:16 -0700

    Added Lodash and used throttle to limit scroll detection.

--- app/assets/scripts/RevealOnScroll.js
+++ app/assets/scripts/RevealOnScroll.js
@@ -1,19 +1,26 @@
+import { throttle } from 'lodash';
+
 class RevealOnScroll{
     constructor(){
         this.itemsToReveal = document.querySelectorAll('.feature-item');
 
         this.hideInitially();
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
         this.events();
     }
 
     events(){
-        window.addEventListener('scroll', () => {
-            this.itemsToReveal.forEach( el => {
-                this.calculateIfScrolledTo(el);
-            });
-        });
+        window.addEventListener('scroll', this.scrollThrottle);
     }
+    
+    
+    calcCaller(){
+        console.log('Scroll function called')
+        this.itemsToReveal.forEach( el => {
+            this.calculateIfScrolledTo(el);
+        });
 
+    }
     calculateIfScrolledTo(el){
         // el.getBoundingClientRect().y isn't supported in the Edge browser
         // Using .top is supported across all major browsers.
