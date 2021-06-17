a7ec0716453135eddee5eb3433d582d243506500
ctmoyer <ctmoyer90@gmail.com>
2021-06-17 16:30:15 -0700

    Completed scroll to reveal Features & Testimonials

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,11 @@ import '../styles/styles.css';
 import MobileMenu from './MobileMenu';
 import RevealOnScroll from './RevealOnScroll';
 
+// const revealOnScroll = new RevealOnScroll();
+new RevealOnScroll(document.querySelectorAll('.feature-item'));
+new RevealOnScroll(document.querySelectorAll('.testimonial'),85);
+
 const mobileMenu = new MobileMenu();
-const revealOnScroll = new RevealOnScroll();
 
 if(module.hot){
     module.hot.accept()
--- app/assets/scripts/RevealOnScroll.js
+++ app/assets/scripts/RevealOnScroll.js
@@ -1,37 +1,62 @@
-import { throttle } from 'lodash';
+// debounce: call function n milliseconds after end of user input/event
+import { throttle, debounce } from 'lodash';
 
 class RevealOnScroll{
-    constructor(){
-        this.itemsToReveal = document.querySelectorAll('.feature-item');
+    constructor(els, threshold = 75){
+        this.itemsToReveal = els;
+        this.threshold = threshold;
 
         this.hideInitially();
         this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
         this.events();
+
+        this.browserHeight = window.innerHeight;
     }
 
     events(){
+        // The named function 'this.scrollThrottle' is used as a reference
+        // to this particular event listener
         window.addEventListener('scroll', this.scrollThrottle);
+        window.addEventListener('resize', debounce(()=>{
+            console.log('debounce fired');
+            this.browserHeight = window.innerHeight;
+        }, 333));
     }
     
     
     calcCaller(){
-        console.log('Scroll function called')
         this.itemsToReveal.forEach( el => {
-            this.calculateIfScrolledTo(el);
+            if( !(el.isRevealed) ){
+                this.calculateIfScrolledTo(el);
+            }
         });
-
+        
     }
     calculateIfScrolledTo(el){
-        // el.getBoundingClientRect().y isn't supported in the Edge browser
-        // Using .top is supported across all major browsers.
-        let scrollPercent = ( el.getBoundingClientRect().top / window.innerHeight ) * 100;
-        if( scrollPercent < 75 ){
-            el.classList.add('reveal-item--is-visible');
+        if( window.scrollY + this.browserHeight > el.offsetTop ){
+            // el.getBoundingClientRect().y isn't supported in the Edge browser
+            // Using .top is supported across all major browsers.
+            let scrollPercent = ( el.getBoundingClientRect().top / this.browserHeight ) * 100;
+
+            // console.log(`Scroll Percent: ${scrollPercent}\tThreshold: ${this.threshold}`);
+            
+            if( scrollPercent < this.threshold ){
+                el.classList.add('reveal-item--is-visible');
+                el.isRevealed = true;
+                if(el.isLastItem){
+                    // We reference the named function to remove this listener 
+                    window.removeEventListener('scroll', this.scrollThrottle)
+                }
+            }    
         }
     }
 
     hideInitially(){
-        this.itemsToReveal.forEach( el => el.classList.add('reveal-item') );
+        this.itemsToReveal.forEach( el => {
+            el.classList.add('reveal-item');
+            el.isRevealed = false;
+        });
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
     }
 }
 
