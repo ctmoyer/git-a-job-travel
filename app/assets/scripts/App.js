8ab89fcb7f4286c14b926b55d15e85fab8788370
ctmoyer <ctmoyer90@gmail.com>
2021-06-17 13:17:11 -0700

    Reveal on scroll, Greedy, is complete for the Our Features section.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
-import '../styles/styles.css'
-import MobileMenu from './MobileMenu'
+import '../styles/styles.css';
+import MobileMenu from './MobileMenu';
+import RevealOnScroll from './RevealOnScroll';
 
 const mobileMenu = new MobileMenu();
+const revealOnScroll = new RevealOnScroll();
 
 if(module.hot){
     module.hot.accept()
