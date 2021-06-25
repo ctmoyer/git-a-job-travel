b4c3c3251d5a975c3f9d92ee61f349b4fba3fc18
ctmoyer <ctmoyer90@gmail.com>
2021-06-25 14:57:35 -0700

    Updated cloud function to check user input and return an output accordingly.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,36 @@
+/*
+    This file would normally be in a private repo,
+    or hosted in a seperate private repo from the
+    main public facing code. 
+    For the purposes of showcasing this project
+    this will remain public. 
+    This feature and code may be removed at any time.
+*/
+
 exports.handler = function( event, context, callback ){
-    callback(null, { 
-        statusCode: 200,
-        body: 'Welcome to the super secret area'
-    });
+    const password = 'javascript';
+    const secretContent = `
+    <h3>Welcome to the Secret Area!</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and that grass is <strong>green</strong>.
+    `
+
+    let body;
+
+    if(event.body){
+        body = JSON.parse(event.body);
+    }else{
+        body = {}
+    }
+
+    if( body.password == password){
+        callback(null, { 
+            statusCode: 200,
+            body: secretContent
+        });
+    }else{
+        callback(null, { 
+            statusCode: 401,
+        });
+    }
+
 }
